d50913461f0f9d06db8765b9fef65093459a5864
JoeyJu <100300405+joeyedward@users.noreply.github.com>
2024-10-09 23:13:11 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker.joeycloud.cn" + CUSTOM_DOMAIN]: dockerHub,
+  ["quay.joeycloud.cn" + CUSTOM_DOMAIN]: "https://quay.io",
+  ["gcr.joeycloud.cn" + CUSTOM_DOMAIN]: "https://gcr.io",
+  ["k8s-gcr.joeycloud.cn" + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
+  ["k8s.joeycloud.cn" + CUSTOM_DOMAIN]: "https://registry.k8s.io",
+  ["ghcr.joeycloud.cn" + CUSTOM_DOMAIN]: "https://ghcr.io",
+  ["cloudsmith.joeycloud.cn" + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
+  ["ecr.joeycloud.cn" + CUSTOM_DOMAIN]: "https://public.ecr.aws",
 
   // staging
   ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
